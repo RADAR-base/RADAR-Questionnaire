b0b67d88d4bfd847cce21c598accfcc7aa304f03
Nithin Thomas <Nithin Thomas>
2017-03-14 11:31:46 +0100

    Modified code as google coding style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
-import { IonicApp, IonicModule } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { QuestionsPage } from '../pages/questions/questions';
-import { QuestionComponent } from '../components/question/question';
-import { QuestionService } from '../providers/question-service';
-import { AnswerService } from '../providers/answer-service';
-import { RangeInputComponent } from '../components/range-input/range-input';
-import { RadioInputComponent } from '../components/radio-input/radio-input';
-import { FinishPage } from '../pages/finish/finish';
-import { SliderInputComponent } from '../components/slider-input/slider-input';
-import { AudioInputComponent } from '../components/audio-input/audio-input';	//added audio-input component
+import { NgModule } from '@angular/core'
+import { IonicApp, IonicModule } from 'ionic-angular'
+import { MyApp } from './app.component'
+import { HomePage } from '../pages/home/home'
+import { QuestionsPage } from '../pages/questions/questions'
+import { QuestionComponent } from '../components/question/question'
+import { QuestionService } from '../providers/question-service'
+import { AnswerService } from '../providers/answer-service'
+import { RangeInputComponent } from '../components/range-input/range-input'
+import { RadioInputComponent } from '../components/radio-input/radio-input'
+import { FinishPage } from '../pages/finish/finish'
+import { SliderInputComponent } from '../components/slider-input/slider-input'
+import { AudioInputComponent } from '../components/audio-input/audio-input'	//added audio-input component
 
 @NgModule({
   imports: [
@@ -51,7 +51,9 @@ import { AudioInputComponent } from '../components/audio-input/audio-input';	//a
   ],
   providers: [
     QuestionService,
-    AnswerService,
+    AnswerService
   ]
 })
-export class AppModule {}
+export class AppModule {
+  
+}

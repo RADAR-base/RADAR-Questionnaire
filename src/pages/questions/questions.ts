10765a4e9571c2d67906f667bd94bbb694b3d7e7
Herculano Campos <hi@herkulano.com>
2017-05-29 16:06:46 +0300

    improve code
    * remove callback hell in checkPermissions
    * use find() instead of while loop to check for audio type
    * tidy up and clean up extraneous code

--- src/pages/questions/questions.ts
+++ src/pages/questions/questions.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core'
-import { App, Content, NavController, NavParams, ViewController, Platform  } from 'ionic-angular'
-import * as opensmile from '../../../plugins/cordova-plugin-opensmile/www/opensmile' //file path to opensmile.js; Adding opensmile plugin
+import { App, Content, NavController, NavParams, ViewController, Platform } from 'ionic-angular'
+import * as opensmile from '../../../plugins/cordova-plugin-opensmile/www/opensmile'
 import { Device } from '@ionic-native/device'
 import { Question } from '../../models/question'
 import { AnswerService } from '../../providers/answer-service'
@@ -8,7 +8,6 @@ import { FinishPage } from '../finish/finish'
 
 declare var cordova: any
 declare var window: any
-
 
 @Component({
   selector: 'page-questions',
@@ -34,27 +33,29 @@ export class QuestionsPage {
     finish: 'FINISH',
     close: 'CLOSE'
   }
-  nextBtTxt: string = this.txtValues.next
-  previousBtTxt: string = this.txtValues.close
+
+  nextBtTxt = this.txtValues.next
+  previousBtTxt = this.txtValues.close
   isNextBtDisabled = true
   isPreviousBtVisible = false
-  platform: boolean = false
-  permission: boolean = false
-  audio: boolean = false
-  permissions = null
-  fileName: string = null
-  questionID: string = null
-  audioRecordStatus: boolean = false;
+  platform = false
+  permission = false
+  audio = false
+  fileName: string
+  questionID
+  audioRecordStatus = false
+
   answer = {
-      id: null,
-      value: null
+    id: null,
+    value: null
   }
 
   iconValues = {
     previous: 'ios-arrow-back',
     close: 'close-circle'
   }
-  iconPrevious: string = this.iconValues.close
+
+  iconPrevious = this.iconValues.close
 
   constructor (
     public navCtrl: NavController,
@@ -62,160 +63,141 @@ export class QuestionsPage {
     public viewCtrl: ViewController,
     public appCtrl: App,
     private answerService: AnswerService,
-    appPlatform: Platform,
+    private appPlatform: Platform,
     private device: Device
   ) {
-    //Stop opensmile when application is on pause
+    // Stop opensmile when application is on pause
     document.addEventListener('pause', () => {
-        this.stopOpensmile()
-    });
-    //Stop opensmile when back button is pressed
-    document.addEventListener("backbutton", () => {
-        this.stopOpensmile()
-    });
+      this.stopOpensmile()
+    })
+    // Stop opensmile when back button is pressed
+    document.addEventListener('backbutton', () => {
+      this.stopOpensmile()
+    })
   }
 
   ionViewDidLoad () {
     this.questions = this.navParams.data
     this.questionsContainerEl = this.questionsContainerRef.nativeElement
-    var i = 0
-    //Checking whether questionnaire contains any audio-type questions
-    while (i < this.questions.length) {
-      if (this.questions[i].type == 'audio') {
-        this.audio = true
-         break
-      }
-      i = i + 1
+
+    // Checking whether questionnaire contains any audio-type questions
+    if (this.questions.find(question => question.type === 'audio')) {
+      this.audio = true
     }
-    //Checking for platform and permission
-    if (this.device.platform == 'Android') {
-      this.permissions = cordova.plugins.permissions
+
+    // Checking for platform and permission
+    if (this.device.platform === 'Android') {
       this.platform = true
+
       // Checking permissions only if platform is android
-      if (this.audio == true) {
-        this.checkPermissionAudio()
+      if (this.audio === true) {
+        this.checkPermissions()
       }
     } else {
       this.platform = false
     }
+
     this.setCurrentQuestion()
   }
 
-  //Checking permission for audio recording and external storage
-  checkPermissionAudio() {
-    this.permissions.hasPermission(this.permissions.RECORD_AUDIO,
-    (status) => {
-      if (!status.hasPermission) {
-        var errorCallback = function () {
-          alert('Permission not set')
-        }
-        this.permissions.requestPermissions([this.permissions.RECORD_AUDIO,this.permissions.WRITE_EXTERNAL_STORAGE],
-        (status) => {
-          if (!status.hasPermission) {
-            errorCallback()
-          } else {
-            this.permission = true;
-          }
-        },
-        errorCallback)
+  // Checking permission for audio recording and external storage
+  checkPermissions () {
+    const permissions = cordova.plugins.permissions
+    const permissionList = [
+      permissions.RECORD_AUDIO,
+      permissions.WRITE_EXTERNAL_STORAGE
+    ]
+
+    permissions.requestPermissions(permissionList, (status) => {
+      console.log('requestPermission 2', status, permissionList)
+      if (status.hasPermission) {
+        this.permission = true
       } else {
-        this.permissions.hasPermission(this.permissions.WRITE_EXTERNAL_STORAGE,
-        (status) => {
-          if (!status.hasPermission) {
-            var errorCallback = function () {
-              alert('Permission not set')
-            }
-            this.permissions.requestPermission(this.permissions.WRITE_EXTERNAL_STORAGE,
-            (status) => {
-              if (!status.hasPermission) {
-                errorCallback()
-              } else {
-                this.permission = true;
-              }
-            },
-            errorCallback)
-          } else {
-            this.permission = true;
-          }
-        }, null);
+        errorCallback('could not set permission')
       }
-    }, null);
+    }, errorCallback)
+
+    function errorCallback (error) {
+      console.error(error)
+      alert('Permission not set')
+    }
   }
 
-  success(message) {
+  success (message) {
+    // add message
   }
 
-  failure() {
+  failure () {
     alert('Error calling OpenSmile Plugin')
   }
 
-  setFileName(file_name) {
-    this.fileName = file_name
+  setFileName (fileName) {
+    this.fileName = fileName
   }
 
-  setQuestionID(qid) {
+  setQuestionID (qid) {
     this.questionID = qid
   }
 
-  setAudioRecordStatus(status) {
+  setAudioRecordStatus (status) {
     this.audioRecordStatus = status
   }
-  getAudioRecordStatus() {
+
+  getAudioRecordStatus () {
     return this.audioRecordStatus
   }
 
-  stopOpensmile() {
-    if(this.getAudioRecordStatus()) {
+  stopOpensmile () {
+    if (this.getAudioRecordStatus()) {
       opensmile.stop('Stop', this.success, this.failure)
       this.setAudioRecordStatus(false)
       this.readFile()
     }
   }
 
-  readFile() {
-    var ans_b64 = null
+  readFile () {
+    let ansB64 = null
     window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory + '/' + this.fileName, (fileEntry) => {
-      fileEntry.file( (file) => {
-        var reader = new FileReader()
+      fileEntry.file((file) => {
+        const reader = new FileReader()
         reader.onloadend = (e: any) => {
-          ans_b64 = e.target.result
+          ansB64 = e.target.result
           this.answer.id = this.questionID
-          var displayDate = new Date()
-          var date = displayDate.toISOString()
-				  this.answer.value = ans_b64
+          const displayDate = new Date()
+          const date = displayDate.toISOString()
+          this.answer.value = ansB64
           this.answerService.add(this.answer)
-        };
+        }
         reader.readAsDataURL(file)
       }, errorCallback)
-    }, errorCallback);
-    function errorCallback(error) {
-      alert("ERROR: " + error.code)
+    }, errorCallback)
+    function errorCallback (error) {
+      alert('ERROR: ' + error.code)
     }
   }
   setCurrentQuestion (value = 0) {
-    //Checking for platform and if it is not android, audio questions will be skipped, don't show it to user
-		if (this.platform == false) {
-			while (this.questions[this.currentQuestion + value].type == 'audio') {
-				this.answer.id = this.questions[this.currentQuestion + value].id
-				this.answer.value = 'Platform not supported'
-				this.answerService.add(this.answer)
-				if (value <= -1) {
-					value = value - 1
-				} else {
-					value = value + 1
-				}
-				if (this.currentQuestion + value < 0) {
-					value = 0
-				}
-				if (this.currentQuestion + value == this.questions.length) {
-					break
-				}
-			}
-		}
-		if ((this.questions[this.currentQuestion].type == 'audio'))
-		{
+    // Checking for platform and if it is not android, audio questions will be skipped, don't show it to user
+    if (this.platform === false) {
+      while (this.questions[this.currentQuestion + value].type === 'audio') {
+        this.answer.id = this.questions[this.currentQuestion + value].id
+        this.answer.value = 'Platform not supported'
+        this.answerService.add(this.answer)
+        if (value <= -1) {
+          value = value - 1
+        } else {
+          value = value + 1
+        }
+        if (this.currentQuestion + value < 0) {
+          value = 0
+        }
+        if (this.currentQuestion + value === this.questions.length) {
+          break
+        }
+      }
+    }
+    if ((this.questions[this.currentQuestion].type === 'audio')) {
       this.stopOpensmile()
-		}
+    }
     const min = !(this.currentQuestion + value < 0)
     const max = !(this.currentQuestion + value >= this.questions.length)
     const finish = (this.currentQuestion + value === this.questions.length)
@@ -257,7 +239,7 @@ export class QuestionsPage {
     this.progress = percent + tick
   }
 
-  getPermission() {
+  getPermission () {
     return this.permission
   }
 

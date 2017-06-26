994b6d16dd9b8826898851d305d048fdaa99207c
Maximilian Kerz <kerz.maximilian@gmail.com>
2017-06-26 20:26:06 +0100

    feature(task-calendar component) added

---
+++ src/components/task-calendar/task-calendar.ts
@@ -0,0 +1,18 @@
+import { Component, OnChanges,  } from '@angular/core'
+import { NavController } from 'ionic-angular'
+
+
+@Component({
+  selector: 'task-calendar',
+  templateUrl: 'task-calendar.html'
+})
+export class TaskCalendarComponent implements OnChanges {
+
+
+  constructor(){
+  }
+
+  ngOnChanges (changes) {
+  }
+
+}

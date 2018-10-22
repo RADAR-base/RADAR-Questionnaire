b9a66df6b595e8609bdeadcdeff818ed39c5c969
pauline <mpgxvii@gmail.com>
2018-10-22 17:33:06 +0100

    fix url regex

--- src/pages/enrolment/enrolment.ts
+++ src/pages/enrolment/enrolment.ts
@@ -45,9 +45,8 @@ export class EnrolmentPage {
   outcomeStatus: String
   reportSettings: WeeklyReportSubSettings[] = DefaultSettingsWeeklyReport
 
-  URLRegEx =
-    // tslint:disable-next-line:max-line-length
-    '^(?:(?:https?|ftp)://)?(?:(?!(?:10|127)(?:.d{1,3}){3})(?!(?:169.254|192.168)(?:.d{1,3}){2})(?!172.(?:1[6-9]|2d|3[0-1])(?:.d{1,3}){2})(?:[1-9]d?|1dd|2[01]d|22[0-3])(?:.(?:1?d{1,2}|2[0-4]d|25[0-5])){2}(?:.(?:[1-9]d?|1dd|2[0-4]d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:.(?:[a-z\u00a1-\uffff]{2,})))(?::d{2,5})?(?:/S*)?$'
+  URLRegEx = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?'
+
   language: LanguageSetting = {
     label: LocKeys.LANGUAGE_ENGLISH.toString(),
     value: 'en'

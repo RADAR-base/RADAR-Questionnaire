bf9a0194e10efb85740e96bba0d3c69d8a6712dc
pauline <mpgxvii@gmail.com>
2018-10-25 17:27:09 +0100

    fix evaluation of branching logic

--- src/app/pages/questions/containers/questions-page.component.ts
+++ src/app/pages/questions/containers/questions-page.component.ts
@@ -1,3 +1,4 @@
+// tslint:disable:no-eval
 import { Component, ElementRef, ViewChild } from '@angular/core'
 import {
   App,
@@ -204,16 +205,32 @@ export class QuestionsPageComponent {
   evalSkipNext() {
     let increment = 1
     let questionIdx = this.currentQuestion + 1
+    const questionFieldName = this.questions[this.currentQuestion].field_name
+    let answerLength = this.answerService.answers[questionFieldName].length
+
     if (questionIdx < this.questions.length) {
       while (this.questions[questionIdx].evaluated_logic !== '') {
         const logic = this.questions[questionIdx].evaluated_logic
         const responses = this.answerService.answers
-        // tslint:disable-next-line:no-eval
-        if (eval(logic) === false) {
+        if (answerLength) {
+          while (answerLength > 0) {
+            responses[questionFieldName] = this.answerService.answers[
+              questionFieldName
+            ][answerLength - 1]
+            if (eval(logic) === true) {
+              return increment
+            }
+            answerLength--
+          }
           increment += 1
           questionIdx += 1
         } else {
-          return increment
+          if (eval(logic) === false) {
+            increment += 1
+            questionIdx += 1
+          } else {
+            return increment
+          }
         }
       }
       return increment

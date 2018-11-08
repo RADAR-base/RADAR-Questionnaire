2b06e6837ca1fb6f101550d3f80fb648b104a8e2
pauline <16977973+mpgxvii@users.noreply.github.com>
2018-11-08 14:46:36 +0000

    use enums

--- src/app/shared/utilities/time.ts
+++ src/app/shared/utilities/time.ts
@@ -1,10 +1,12 @@
-const SEC_MILLISEC = 1000
-const HOUR_MIN = 60
-const MIN_SEC = 60
-const DAY_HOUR = 24
-const YEAR_DAY = 365
-const WEEK_DAY = 7
-const MONTH_DAY = 31
+export enum TimeConversion {
+  SEC_TO_MILLISEC = 1000,
+  HOUR_TO_MIN = 60,
+  MIN_TO_SEC = 60,
+  DAY_TO_HOUR = 24,
+  YEAR_TO_DAY = 365,
+  WEEK_TO_DAY = 7,
+  MONTH_TO_DAY = 31
+}
 
 interface Time {
   years?: number
@@ -18,21 +20,44 @@ interface Time {
 }
 
 export function getMilliseconds(time: Time) {
-  return getSeconds(time) * SEC_MILLISEC
+  return getSeconds(time) * TimeConversion['SEC_TO_MILLISEC']
 }
 
 export function getSeconds(time: Time) {
   let seconds = 0
   if (time.years)
-    seconds += time.years * YEAR_DAY * DAY_HOUR * HOUR_MIN * MIN_SEC
+    seconds +=
+      time.years *
+      TimeConversion['YEAR_TO_DAY'] *
+      TimeConversion['DAY_TO_HOUR'] *
+      TimeConversion['HOUR_TO_MIN'] *
+      TimeConversion['MIN_TO_SEC']
   if (time.months)
-    seconds += time.months * MONTH_DAY * DAY_HOUR * HOUR_MIN * MIN_SEC
+    seconds +=
+      time.months *
+      TimeConversion['MONTH_TO_DAY'] *
+      TimeConversion['DAY_TO_HOUR'] *
+      TimeConversion['HOUR_TO_MIN'] *
+      TimeConversion['MIN_TO_SEC']
   if (time.weeks)
-    seconds += time.weeks * WEEK_DAY * DAY_HOUR * HOUR_MIN * MIN_SEC
-  if (time.days) seconds += time.days * DAY_HOUR * HOUR_MIN * MIN_SEC
-  if (time.hours) seconds += time.hours * HOUR_MIN * MIN_SEC
-  if (time.minutes) seconds += time.minutes * MIN_SEC
+    seconds +=
+      time.weeks *
+      TimeConversion['WEEK_TO_DAY'] *
+      TimeConversion['DAY_TO_HOUR'] *
+      TimeConversion['HOUR_TO_MIN'] *
+      TimeConversion['MIN_TO_SEC']
+  if (time.days)
+    seconds +=
+      time.days *
+      TimeConversion['DAY_TO_HOUR'] *
+      TimeConversion['HOUR_TO_MIN'] *
+      TimeConversion['MIN_TO_SEC']
+  if (time.hours)
+    seconds +=
+      time.hours * TimeConversion['HOUR_TO_MIN'] * TimeConversion['MIN_TO_SEC']
+  if (time.minutes) seconds += time.minutes * TimeConversion['MIN_TO_SEC']
   if (time.seconds) seconds += time.seconds
-  if (time.milliseconds) seconds += time.milliseconds / SEC_MILLISEC
+  if (time.milliseconds)
+    seconds += time.milliseconds / TimeConversion['SEC_TO_MILLISEC']
   return seconds
 }

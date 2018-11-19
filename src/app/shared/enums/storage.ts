231fd275cd4f418d61b2700f921abcb1529ae261
pauline <16977973+mpgxvii@users.noreply.github.com>
2018-11-19 17:50:11 +0000

    add completed tasks to storage

--- src/app/shared/enums/storage.ts
+++ src/app/shared/enums/storage.ts
@@ -15,6 +15,7 @@ export class StorageKeys {
   static SCHEDULE_VERSION = new StorageKeys('SCHEDULE_VERSION')
   static SCHEDULE_TASKS = new StorageKeys('SCHEDULE_TASKS')
   static SCHEDULE_TASKS_CLINICAL = new StorageKeys('SCHEDULE_TASKS_CLINICAL')
+  static SCHEDULE_TASKS_COMPLETED = new StorageKeys('SCHEDULE_TASKS_COMPLETED')
   static SCHEDULE_REPORT = new StorageKeys('SCHEDULE_REPORT')
   static CACHE_ANSWERS = new StorageKeys('CHACHE_ANSWERS')
   static HAS_CLINICAL_TASKS = new StorageKeys('HAS_CLINICAL_TASKS')

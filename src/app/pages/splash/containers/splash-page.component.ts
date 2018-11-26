439b4d82f0004d04bd94ee0649f8bbc22780a444
pauline <16977973+mpgxvii@users.noreply.github.com>
2018-11-26 12:01:28 +0000

    fix updateconfigstatetimezone

--- src/app/pages/splash/containers/splash-page.component.ts
+++ src/app/pages/splash/containers/splash-page.component.ts
@@ -57,9 +57,7 @@ export class SplashPageComponent {
               )
               this.storage.set(StorageKeys.TIME_ZONE, res.timezone)
               this.storage.set(StorageKeys.UTC_OFFSET, res.utc_offset)
-              return timeZone
-                ? this.configService.updateConfigStateOnTimezoneChange()
-                : this.configService.fetchConfigState(true)
+              this.configService.updateConfigStateOnTimezoneChange()
             } else {
               console.log('[SPLASH] Current Timezone is ' + timeZone)
             }

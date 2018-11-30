8e5ef53f5e4fc17b7b8dee7c983940a89d85cf58
pauline <16977973+mpgxvii@users.noreply.github.com>
2018-11-30 17:38:38 +0000

    remove unnecessary functions

--- src/app/pages/clinical-tasks/services/clinical-tasks.service.ts
+++ src/app/pages/clinical-tasks/services/clinical-tasks.service.ts
@@ -7,33 +7,7 @@ import { StorageKeys } from '../../../shared/enums/storage'
 export class ClinicalTasksService {
   constructor(public storage: StorageService) {}
 
-  getClinicalAssessment(task) {
-    return this.storage.getClinicalAssessment(task)
-  }
-
-  getClinicalTasks() {
+  getClinicalAssessments() {
     return this.storage.get(StorageKeys.CONFIG_CLINICAL_ASSESSMENTS)
-  }
-
-  getClinicalTasksUpdated() {
-    const tasks = this.getClinicalTasks()
-    const completed = this.getCompletedClinicalTasks()
-    return Promise.all([tasks, completed]).then(res => {
-      const clinicalAssessments = res[0]
-      const completedTasks = res[1]
-      if (completedTasks) {
-        completedTasks.map(d => {
-          const index = clinicalAssessments.findIndex(s => s.name == d.name)
-          if (index > -1) {
-            clinicalAssessments[index].completed = true
-          }
-        })
-      }
-      return clinicalAssessments
-    })
-  }
-
-  getCompletedClinicalTasks() {
-    return this.storage.get(StorageKeys.SCHEDULE_TASKS_COMPLETED)
   }
 }

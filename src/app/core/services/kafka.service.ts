df388ccfa5b2bf8e6f33d6574525cc6fb978d15d
pauline <16977973+mpgxvii@users.noreply.github.com>
2018-12-03 19:51:51 +0000

    add preparation of audio kafka object

--- src/app/core/services/kafka.service.ts
+++ src/app/core/services/kafka.service.ts
@@ -7,6 +7,7 @@ import * as KafkaRest from 'kafka-rest'
 import {
   DefaultEndPoint,
   KAFKA_ASSESSMENT,
+  KAFKA_AUDIO,
   KAFKA_CLIENT_KAFKA,
   KAFKA_COMPLETION_LOG,
   KAFKA_TIMEZONE,
@@ -49,21 +50,37 @@ export class KafkaService {
 
   prepareAnswerKafkaObjectAndSend(task: Task, data, questions) {
     // NOTE: Payload for kafka 1 : value Object which contains individual questionnaire response with timestamps
-    const Answer: AnswerValueExport = {
-      name: task.name,
-      version: data.configVersion,
-      answers: data.answers,
-      time:
-        questions[0].field_type == QuestionType.info && questions[1] // NOTE: Do not use info startTime
-          ? data.answers[1].startTime
-          : data.answers[0].startTime, // NOTE: whole questionnaire startTime and endTime
-      timeCompleted: data.answers[data.answers.length - 1].endTime,
-      timeNotification: task.timestamp
-        ? { double: task.timestamp / SEC_MILLISEC }
-        : null
+    const time =
+      questions[0].field_type == QuestionType.info && questions[1] // NOTE: Do not use info startTime
+        ? data.answers[1].startTime
+        : data.answers[0].startTime // NOTE: whole questionnaire startTime and endTime
+    const timeNotification = task.timestamp
+      ? { double: task.timestamp / SEC_MILLISEC }
+      : null
+    const timeCompleted = data.answers[data.answers.length - 1].endTime
+    if (task.name !== KAFKA_AUDIO) {
+      const Answer: AnswerValueExport = {
+        time: time,
+        timeCompleted: timeCompleted,
+        timeNotification: timeNotification,
+        name: task.name,
+        version: data.configVersion,
+        answers: data.answers
+      }
+      return this.prepareKafkaObjectAndSend(task, Answer, KAFKA_ASSESSMENT)
+    } else {
+      const Answer = {
+        time: time,
+        timeCompleted: timeCompleted,
+        timeNotification: timeNotification,
+        mediaType: 'audio/wav',
+        data: { string: data.answers[1].value },
+        reciteText: questions[1].select_choices_or_calculations
+          .map(d => d.label)
+          .toString()
+      }
+      return this.prepareKafkaObjectAndSend(task, Answer, KAFKA_AUDIO)
     }
-
-    return this.prepareKafkaObjectAndSend(task, Answer, KAFKA_ASSESSMENT)
   }
 
   prepareNonReportedTasksKafkaObjectAndSend(task: Task) {
@@ -95,6 +112,7 @@ export class KafkaService {
   getSpecs(task: Task, kafkaObject, type) {
     switch (type) {
       case KAFKA_ASSESSMENT:
+      case KAFKA_AUDIO:
         return this.storage.getAssessmentAvsc(task).then(specs => {
           return Promise.resolve(
             Object.assign({}, { task: task, kafkaObject: kafkaObject }, specs)

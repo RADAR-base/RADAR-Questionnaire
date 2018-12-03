bb83a2d8a437fcaf2a46d2de5c59706f83f5dcdc
pauline <16977973+mpgxvii@users.noreply.github.com>
2018-12-03 19:53:35 +0000

    fix audio record service

--- src/app/pages/questions/services/audio-record.service.ts
+++ src/app/pages/questions/services/audio-record.service.ts
@@ -7,11 +7,7 @@ declare var cordova: any
 declare var window: any
 @Injectable()
 export class AudioRecordService {
-  filename: string = 'audio-opensmile.bin'
-
   audioRecordStatus: boolean = false
-  recording: boolean = false
-  recordingTime: number = 45000
 
   constructor(private file: File) {
     // NOTE: Kill recording on load
@@ -21,23 +17,20 @@ export class AudioRecordService {
   setAudioRecordStatus(status) {
     this.audioRecordStatus = status
   }
+
   getAudioRecordStatus() {
     return this.audioRecordStatus
   }
 
-  startAudioRecording(configFile) {
-    this.recording = this.getAudioRecordStatus()
-
-    if (this.recording === false) {
-      this.recording = true
-      this.setRecordTimer()
-      this.setAudioRecordStatus(this.recording)
-      opensmile.start(this.filename, configFile, this.success, this.failure)
-    } else if (this.recording === true) {
-      this.recording = false
-      this.setAudioRecordStatus(this.recording)
+  startAudioRecording(fileName, configFile) {
+    const recording = this.getAudioRecordStatus()
+    if (recording === false) {
+      this.setAudioRecordStatus(true)
+      opensmile.start(fileName, configFile, this.success, this.failure)
+    } else if (recording === true) {
+      this.setAudioRecordStatus(false)
       opensmile.stop('Stop', this.success, this.failure)
-      this.readAudioFile(this.filename)
+      this.readAudioFile(fileName)
     }
   }
 
@@ -45,17 +38,6 @@ export class AudioRecordService {
     if (this.getAudioRecordStatus()) {
       opensmile.stop('Stop', this.success, this.failure)
       this.setAudioRecordStatus(false)
-      this.readAudioFile(this.filename)
-    }
-  }
-
-  setRecordTimer() {
-    if (this.recording === true) {
-      setTimeout(() => {
-        console.log('Time up for recording')
-        this.recording = false
-        this.stopAudioRecording()
-      }, this.recordingTime)
     }
   }
 
@@ -72,39 +54,6 @@ export class AudioRecordService {
   }
 
   readAudioFile(filename) {
-    const filePath = this.getPath()
-    console.log(filePath)
-    this.file.readAsDataURL(filePath, filename).then(
-      base64 => {
-        console.log(base64)
-      },
-      error => {
-        console.log(error)
-      }
-    )
+    return this.file.readAsDataURL(this.getPath(), filename)
   }
 }
-
-/*****************************
-
-readAudioFile(filename) {
-  var ans_b64 = null
-  window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory + '/' + filename, (fileEntry) => {
-    fileEntry.file((file) => {
-      var reader = new FileReader()
-      reader.onloadend = (e: any) => {
-        ans_b64 = e.target.result
-        console.log(ans_b64)
-        this.answer.id = this.questionID
-        this.answer.value = ans_b64
-        this.answerService.add(this.answer)
-      };
-      reader.readAsDataURL(file)
-    }, errorCallback)
-  }, errorCallback);
-  function errorCallback(error) {
-    alert("ERROR: " + error.code)
-  }
-}
-
-*********************************/

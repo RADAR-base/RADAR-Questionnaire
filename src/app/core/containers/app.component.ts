5e50b87df9c9a70fa0b5d8734e00a73a60342f50
pauline <16977973+mpgxvii@users.noreply.github.com>
2019-02-22 11:20:04 +0000

    fix: demo, accessibility

--- src/app/core/containers/app.component.ts
+++ src/app/core/containers/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { MobileAccessibility } from '@ionic-native/mobile-accessibility/ngx'
 import { SplashScreen } from '@ionic-native/splash-screen/ngx'
 import { StatusBar } from '@ionic-native/status-bar/ngx'
 import { Platform } from 'ionic-angular'
@@ -17,9 +18,11 @@ export class AppComponent {
     private platform: Platform,
     private statusBar: StatusBar,
     private splashScreen: SplashScreen,
+    private accessibility: MobileAccessibility,
     private notificationService: NotificationService
   ) {
     this.platform.ready().then(() => {
+      this.accessibility.usePreferredTextZoom(false)
       this.statusBar.hide()
       this.splashScreen.hide()
       this.notificationService.init()

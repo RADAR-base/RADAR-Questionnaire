f251a13fd94e2e7d2d8a91dd86477b363c5915c4
pauline <16977973+mpgxvii@users.noreply.github.com>
2019-05-13 14:51:50 +0100

    fix: add range-info-input type

--- src/app/pages/questions/components/question/range-info-input/range-info-input.component.ts
+++ src/app/pages/questions/components/question/range-info-input/range-info-input.component.ts
@@ -5,42 +5,46 @@ import { Item, Response } from '../../../../../shared/models/question'
 let uniqueID = 0
 
 @Component({
-  selector: 'radio-input',
-  templateUrl: 'radio-input.component.html'
+  selector: 'range-info-input',
+  templateUrl: 'range-info-input.component.html'
 })
-export class RadioInputComponent implements OnInit {
+export class RangeInfoInputComponent implements OnInit {
   @Output()
   valueChange: EventEmitter<number> = new EventEmitter<number>()
 
   @Input()
-  responses: Response[]
+  min = 1
+  @Input()
+  max = 10
+  @Input()
+  labelLeft = ''
+  @Input()
+  labelRight = ''
   @Input()
   currentlyShown
+  @Input()
+  responses: Response[]
 
   value: number = null
   uniqueID: number = uniqueID++
-  name = `radio-input-${this.uniqueID}`
+  name = `range-input-${this.uniqueID}`
   items: Item[] = Array()
+  itemDescription: string
 
   ngOnInit() {
-    this.responses.map((item, i) => {
-      const codeChecked = this.checkCode(item.code)
+    for (let i = this.min; i <= this.max; i++) {
       this.items.push({
-        id: `radio-${this.uniqueID}-${i}`,
-        response: item.label,
-        value: codeChecked
+        id: `range-${this.uniqueID}-${i}`,
+        value: i
       })
-    })
-  }
-
-  checkCode(code) {
-    if (code.includes('\r')) {
-      return code.substr(2)
     }
-    return code
   }
 
   onInputChange(event) {
     this.valueChange.emit(+event.target.value)
+  }
+
+  showDescription(id) {
+    this.itemDescription = this.responses[id].label
   }
 }

f45c8e4c10c105ec6500d130876e958ef138eefd
pauline <16977973+mpgxvii@users.noreply.github.com>
2019-05-14 12:58:36 +0100

    fix: extend from range input component

--- src/app/pages/questions/components/question/range-info-input/range-info-input.component.ts
+++ src/app/pages/questions/components/question/range-info-input/range-info-input.component.ts
@@ -1,29 +1,21 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core'
+import { Component, Input } from '@angular/core'
 
 import { Response } from '../../../../../shared/models/question'
+import { RangeInputComponent } from '../range-input/range-input.component'
 
 @Component({
   selector: 'range-info-input',
   templateUrl: 'range-info-input.component.html'
 })
-export class RangeInfoInputComponent {
-  @Output()
-  valueChange: EventEmitter<number> = new EventEmitter<number>()
-
-  @Input()
-  min = 1
-  @Input()
-  max = 10
-  @Input()
-  currentlyShown
+export class RangeInfoInputComponent extends RangeInputComponent {
   @Input()
   responses: Response[]
 
   itemDescription: string
 
   onInputChange(value) {
+    super.onInputChange(value)
     this.showDescription(value)
-    this.valueChange.emit(value)
   }
 
   showDescription(id) {

dde1b3b2efe608c09bf84119beab755e623a76b4
pauline <16977973+mpgxvii@users.noreply.github.com>
2019-06-05 09:12:21 +0100

    fix: improve css and animations

--- src/app/pages/home/components/task-info/task-info.component.ts
+++ src/app/pages/home/components/task-info/task-info.component.ts
@@ -14,41 +14,42 @@ import { TranslatePipe } from '../../../../shared/pipes/translate/translate'
     trigger('fade', [
       state('false', style({ opacity: '0' })),
       state('true', style({ opacity: '1.0' })),
-      transition('* => *', animate('350ms 50ms ease'))
+      transition('* => *', animate('350ms ease'))
     ]),
     trigger('scaleMinutes', [
       state(
         'false',
-        style({ transform: 'translate3d(-25%, -15%, 0) scale(0.4)' })
+        style({ transform: 'translate3d(48%, -15%, 0) scale(0.45)' })
       ),
-      state('true', style({ transform: 'translate3d(0, 0, 0) scale(1)' })),
-      transition('* => *', animate('350ms 50ms ease'))
+      transition('* => *', animate('350ms ease'))
+    ]),
+    trigger('rotateMeridiem', [
+      state('true', style({ transform: 'rotate(90deg)' })),
+      state('false', style({ transform: 'translate3d(-24%, 50%, 0)' })),
+      transition('* => *', animate('350ms ease'))
+    ]),
+    trigger('moveHour', [
+      state('false', style({ transform: 'translate3d(90%, 0, 0)' })),
+      transition('* => *', animate('350ms ease'))
     ]),
     trigger('alignCenterRightExtraInfo', [
-      state('false', style({ transform: 'translate3d(22%, 0, 0)' })),
+      state('false', style({ transform: 'translate3d(8vw, 0, 0) scale(0.8)' })),
       state('true', style({ transform: 'translate3d(0, 0, 0)' })),
-      transition('* => *', animate('350ms 50ms ease'))
+      transition('* => *', animate('350ms ease'))
     ]),
     trigger('alignCenterRightTime', [
-      state('false', style({ transform: 'translate3d(8%, 0, 0) scale(0.8)' })),
-      state('true', style({ transform: 'translate3d(0, 0, 0)' })),
-      transition('* => *', animate('350ms 50ms ease'))
+      state('false', style({ transform: 'scale(0.9)' })),
+      transition('* => *', animate('350ms ease'))
     ]),
     trigger('moveInProgress', [
-      state(
-        'true',
-        style({ display: 'none', transform: 'translate3d(-150%, 0, 0)' })
-      ),
-      state(
-        'false',
-        style({ display: 'block', transform: 'translate3d(0, 0, 0)' })
-      ),
-      transition('* => *', animate('350ms 50ms ease'))
+      state('true', style({ transform: 'translate3d(-150%, 0, 0)' })),
+      state('false', style({ transform: 'translate3d(0, 0, 0)' })),
+      transition('* => *', animate('350ms ease'))
     ]),
     trigger('alignCenterRightMetrics', [
-      state('false', style({ transform: 'translate3d(110%, 0, 0)' })),
+      state('false', style({ transform: 'translate3d(115%, 0, 0)' })),
       state('true', style({ transform: 'translate3d(0, 0, 0)' })),
-      transition('* => *', animate('350ms 50ms ease'))
+      transition('* => *', animate('400ms ease'))
     ])
   ]
 })
--- src/app/pages/home/containers/home-page.component.ts
+++ src/app/pages/home/containers/home-page.component.ts
@@ -31,12 +31,12 @@ import { TasksService } from '../services/tasks.service'
     trigger('displayCalendar', [
       state('true', style({ transform: 'translateY(0)', opacity: 1 })),
       state('false', style({ transform: 'translateY(100%)', opacity: 0 })),
-      transition('*=>*', animate('350ms 50ms ease'))
+      transition('*=>*', animate('350ms ease'))
     ]),
     trigger('moveProgress', [
       state('true', style({ transform: 'translateY(-100%)', display: 'none' })),
       state('false', style({ transform: 'translateY(0)' })),
-      transition('true=>false', animate('400ms ease'))
+      transition('true=>false', animate('300ms'))
     ])
   ]
 })

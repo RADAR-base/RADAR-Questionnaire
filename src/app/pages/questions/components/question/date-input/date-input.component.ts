555331b174c87a05107de4d0dcc23294e0810e32
mpgxvii <mpgxvii@gmail.com>
2019-08-15 12:00:39 +0100

    fix: css and add placeholder input

--- src/app/pages/questions/components/question/date-input/date-input.component.ts
+++ src/app/pages/questions/components/question/date-input/date-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core'
+import { Component, EventEmitter, Input, Output } from '@angular/core'
 
 @Component({
   selector: 'date-input',
@@ -7,6 +7,9 @@ import { Component, EventEmitter, Output } from '@angular/core'
 export class DateInputComponent {
   @Output()
   valueChange: EventEmitter<number> = new EventEmitter<number>()
+
+  @Input()
+  placeholder = 'Select Date'
 
   onInputChange(date) {
     const epoch = new Date(

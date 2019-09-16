61bc8c230ae866e8486a26dd85a97e5ea00fb699
mpgxvii <mpgxvii@gmail.com>
2019-09-16 17:44:26 +0100

    fix: finish page done button

--- src/app/pages/questions/components/finish/finish.component.ts
+++ src/app/pages/questions/components/finish/finish.component.ts
@@ -23,7 +23,7 @@ export class FinishComponent implements OnInit, OnChanges {
   @Input()
   displayNextTaskReminder = true
   @Input()
-  showDoneButton
+  showDoneButton: boolean
   @Input()
   isShown: boolean
 

2d8f0cbdeacebdd807bb3dc2781adad690c81c97
mpgxvii <mpgxvii@gmail.com>
2019-10-02 11:50:01 +0100

    fix: quotes

--- src/app/pages/questions/components/question/text-input/text-input.component.ts
+++ src/app/pages/questions/components/question/text-input/text-input.component.ts
@@ -80,7 +80,7 @@ export class TextInputComponent implements OnInit {
     this.defaultTimePickerValue = {
       hour: moment.format('hh'),
       minute: moment.format('mm'),
-      second: this.showSeconds ? moment.format('ss') : "00",
+      second: this.showSeconds ? moment.format('ss') : '00',
       ampm: moment.format('A')
     }
   }

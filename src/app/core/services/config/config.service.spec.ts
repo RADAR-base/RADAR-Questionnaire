015b4ce6c3e718eee7971287a8f90dcf80242e59
mpgxvii <mpgxvii@gmail.com>
2019-10-08 19:13:25 +0100

    fix: tests

--- src/app/core/services/config/config.service.spec.ts
+++ src/app/core/services/config/config.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHandler } from '@angular/common/http'
 import { TestBed } from '@angular/core/testing'
 
 import {
@@ -17,12 +18,12 @@ import { LocalizationService } from '../misc/localization.service'
 import { LogService } from '../misc/log.service'
 import { NotificationService } from '../notifications/notification.service'
 import { ScheduleService } from '../schedule/schedule.service'
+import { AnalyticsService } from '../usage/analytics.service'
 import { AppConfigService } from './app-config.service'
 import { ConfigService } from './config.service'
 import { ProtocolService } from './protocol.service'
 import { QuestionnaireService } from './questionnaire.service'
 import { SubjectConfigService } from './subject-config.service'
-import { AnalyticsService } from '../usage/analytics.service';
 
 describe('ConfigService', () => {
   let service
@@ -43,7 +44,9 @@ describe('ConfigService', () => {
           provide: AnalyticsService,
           useClass: FirebaseAnalyticsServiceMock
         },
-        { provide: LogService, useClass: LogServiceMock }
+        { provide: LogService, useClass: LogServiceMock },
+        HttpClient,
+        HttpHandler
       ]
     })
   )

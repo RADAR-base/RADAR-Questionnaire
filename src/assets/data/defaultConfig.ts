0adee72202e0179e3cee989fbd520567d61bedc4
mpgxvii <mpgxvii@gmail.com>
2020-01-13 15:09:56 +0000

    chore: update versions

--- src/assets/data/defaultConfig.ts
+++ src/assets/data/defaultConfig.ts
@@ -13,7 +13,7 @@ import { DefaultSourceProducerAndSecretExport } from './secret'
 export const DefaultPlatformInstance = 'RADAR-CNS'
 
 // *Default app version
-export const DefaultAppVersion = '0.7.3-alpha'
+export const DefaultAppVersion = '0.7.3'
 
 // *Default Android package name
 export const DefaultPackageName = 'org.phidatalab.radar_armt'

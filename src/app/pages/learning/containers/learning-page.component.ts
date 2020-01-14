77d60a4cf2cb64945b8e9e15c4bce1773d76d870
nivethika <nivemaham@gmail.com>
2020-01-14 19:24:51 +0100

    commit learning page changes

--- src/app/pages/learning/containers/learning-page.component.ts
+++ src/app/pages/learning/containers/learning-page.component.ts
@@ -35,72 +35,75 @@ export class LearningPageComponent {
     this.localization.update().then(lang => (this.language = lang))
     this.learningPageItems = [
       {
-        title: "Time Commitment",
-        iconMd: "md-alarm",
+        title: "Visas and immigration",
+        iconMd: "md-plane",
         detail: [
-          "We know your time is valuable. That’s why the HOME app has two types of surveys - one short and one longer. Each time we want you to complete one of these surveys you will be sent a notification to open the HOME app.",
-          "In the short survey, sent either once a week or every three days, you will be asked about your health in the last week. If you have been feeling well, this will take less than 15 seconds to complete. If you have not been well, it will take about 1 min to complete.",
-          "In the longer surveys, sent either once a week or every two weeks, you will be asked about a wider range of health and non-health related questions. These surveys are designed to take less than 5 minutes to complete."
+          `<div class="detail text-justify"> There is one single government website for all your visa and immigration information.</div>`,
+          `<a href="https://www.gov.uk/browse/visas-immigration">https://www.gov.uk/browse/visas-immigration</a>`
         ]
       },
       {
-        title: "Privacy",
-        iconMd: "md-lock",
+        title: "Registering for work",
+        iconMd: "md-briefcase",
         detail: [
-          "We take our obligation to protect your privacy very seriously. All of your information will be kept strictly confidential throughout the research project. It will not be possible to identify you from any of the publications or reports that are based on this study. Any answers you give to the survey questions will go from the app, to a secure server hosted by Amazon, and then straight to UCL."
+          `<div class="detail text-justify">For all information related to work, taxes, benefits and unemployment support, please visit:<a href="http://www.gov.uk/">www.gov.uk</a> or visit your local Job centre Plus for face-to-face advice.  </div> `
         ]
       },
       {
-        title: "Data Collection",
-        iconMd: "md-analytics",
+        title: "Access to healthcare",
+        iconMd: "md-medkit",
         detail: [
-          "The survey questions will cover a wide range of topics that can affect your health and wellbeing, like your nutrition, housing, employment, and experience with your GP or healthcare provider. You may find that some questions do not apply to you or make you feel uncomfortable. You are free to choose ‘no response’ or stop answering the survey at any time by choosing the ‘stop survey’ option. Your data will only be used for academic research by UCL and will not be shared with anyone."
+          `<div class="detail text-justify"><li>All healthcare-related information can be found on the National Healthcare Service (NHS) website.<a href="https://www.nhs.uk/">www.nhs.uk</a></li></div>`,
+          `<div class="detail text-justify"><li>See Public Health England’s webpage for useful advice and guidance on the health needs of migrant patients<a href="https://www.gov.uk/guidance/nhs-entitlements-migrant-health-guide">https://www.gov.uk/guidance/nhs-entitlements-migrant-health-guide</a></li></div> `,
+          `<div class="detail text-justify"><li>Find a local GP via <a href="https://www.nhs.uk/service-search/GP/LocationSearch/4">https://www.nhs.uk/service-search/GP/LocationSearch/4</a></li></div> `,
+          `<div class="detail text-justify"><li>For non-urgent advice about any medical condition visit <a href="https://www.nhs.uk/conditions/">https://www.nhs.uk/conditions/</a></li></div> `,
+          `<div class="detail text-justify"><li><b>Emergency number (Ambulance/Police/Fire): Call 999</b></li></div> `,
+          `<div class="detail text-justify"><li><b>Mental health crisis:</b> Call 111 (for local crisis team) or go to A&E (place of safety)</li></div> `,
+          `<div class="detail text-justify"><li><b>Urgent medical advice number (not an emergency): Call 111 </b></li></div> `,
         ]
       },
       {
-        title: "Benefits",
+        title: "Access to health support",
         iconMd: "md-chatbubbles",
         detail: [
-          "Taking part in this study will help us gain a better understanding of the health of migrants like you in the UK. While there are no immediate benefits for you, every survey that you fill out will increase our understanding of the health and wellbeing of migrants."
+          `<div ><b>Mental health support services</b></div>`,
+          `<div ><li>For advice and support to empower anyone experiencing a mental health problem.<a href="https://www.mind.org.uk/">https://www.mind.org.uk/</a></li></div>`,
+          `<div ><b>Domestic abuse support services</b></div>`,
+          `<div ><li>Advice and support for women, children & men experiencing domestic abuse.</li></div>`,
+          `<div >
+             <ul style="list-style-type: square !important;">
+               <li><a href="https://www.refuge.org.uk/">https://www.refuge.org.uk/</a></li>
+               <li><a href="https://www.womensaid.org.uk/">https://www.womensaid.org.uk/</a></li>
+               <li><a href="https://www.welshwomensaid.org.uk/">https://www.welshwomensaid.org.uk/</a></li>
+               <li><a href="http://www.scottishwomensaid.co.uk/">http://www.scottishwomensaid.co.uk/</a></li>
+               <li><a href="https://www.womensaid.ie/">https://www.womensaid.ie/</a></li>
+             </ul>
+           </div>`,
+          `<div ><b>Children abuse support services</b></div>`,
+          `<div ><li>The National Society for the Prevention of Cruelty to Children - to keep children safe from abuse.<a href="https://www.nspcc.org.uk/">https://www.nspcc.org.uk/</a></li></div>`,
+          `<div ><b>Health care and support for refugees, destitute migrants, sex workers and the homeless</b></div>`,
+          `<div ><li><a href="https://www.doctorsoftheworld.org.uk/">https://www.doctorsoftheworld.org.uk/</a></li></div>`,
+          `<div ><b>LGBT+ support services</b></div>`,
+          `<div ><li>Helpline and other advice and information on all immigration and nationality issues for gay men, lesbians, bisexual, transgender and non-binary/gender fluid people.</li></div>`,
+          `<div >
+             <ul style="list-style-type: square !important;">
+               <li><a href="https://uklgig.org.uk/">https://uklgig.org.uk/</a></li>
+               <li><a href="https://www.stonewall.org.uk/">https://www.stonewall.org.uk/</a></li>
+               <li><a href="https://switchboard.lgbt/">https://switchboard.lgbt/</a></li>
+             </ul>
+           </div>`
         ]
       },
       {
-        title: "Study Findings",
-        iconMd: "md-school",
+        title: "Access to community support",
+        iconMd: "md-contacts",
         detail: [
-          "The results of this study will be written up and published in scientific journals as well as within wider media platforms. We will engage with policymakers to help them make decisions that  better reflect the health needs of migrants like yourself in the UK. Additionally, we will send you summaries of our analysed data every few months via email. The results will also be on the study website (www.homeappstudy.net)."
-        ]
-      },
-      {
-        title: "Disadvantages",
-        iconMd: "md-warning",
-        detail: [
-          "In this study you will be asked questions relating to migration, mental and sexual health which you may find distressing. There is a possibility that completing the surveys could evoke previous negative experiences with your health or migratory experience.",
-          "Before you undertake a survey you will be explained in short about its content and you can then decide not to take part in that particular survey. Following most surveys, we will include information to signpost you to national support related to the survey you have just undertaken and should you become distressed. Additionally, the resource section of the HOME app will have more information about the health and migration services available in the UK.",
-          "We advise you to contact your GP or local mental health services should you feel you need more support. Additionally, the resource section of the HOME app will have more information about the health and migration services available in the UK."
-        ]
-      },
-      {
-        title: "Data Protection",
-        iconMd: "md-paper",
-        detail: [
-          "The controller for this project will be University College London (UCL). The UCL Data Protection Officer provides oversight of UCL activities involving the processing of personal data, and can be contacted at data-protection@ucl.ac.uk. This ‘local’ privacy notice sets out the information that applies to this particular study. Further information on how UCL uses participant information can be found in our ‘general’ privacy notice: For participants in health and care research studies, click here (www.homeappstudy.net).",
-          "The information that is required to be provided to participants under data protection legislation (GDPR and DPA 2018) is provided across both the ‘local’ and ‘general’ privacy notices. The lawful basis that will be used to process your personal data are: ‘Public task’ for personal data and’ Research purposes’ for special category data.",
-          "Your personal data will be processed so long as it is required for the research project. If we are able to anonymise or pseudonymise the personal data you provide we will undertake this, and will endeavour to minimise the processing of personal data wherever possible. If you are concerned about how your personal data is being processed, or if you would like to contact us about your rights, please contact UCL in the first instance at data-protection@ucl.ac.uk."
-        ]
-      },
-      {
-        title: "Follow Up",
-        iconMd: "md-mail",
-        detail: [
-          "Your email address will be included in this research study's registry. We will send a newsletter every few months with information about this study. You can opt out of this registry at any time to stop receiving the newsletter or any other form of correspondence."
-        ]
-      },
-      {
-        title: "Withdrawing",
-        iconMd: "md-log-out",
-        detail: [
-          "Your participation in this study is entirely voluntary. You may withdraw your consent and stop your participation in this study at any time. If you choose to withdraw, we will stop collecting any new data and can destroy all of the data we have previously collected. To withdraw from the study, simply send an email to the research team at r.aldridge@ucl.ac.uk."
+          `<div ><b>Citizens advice</b></div>`,
+          `<div ><li>National service that gives free, confidential information and advice to assist people with housing, money, legal, consumer and other problems. <a href="https://www.citizensadvice.org.uk/">https://www.citizensadvice.org.uk/</a></li></div>`,
+          `<div ><b>Migrant families network</b></div>`,
+          `<div ><li>Help for migrant families in relation to housing and financial support when they have no recourse to public funds. <a href="https://migrantfamilies.nrpfnetwork.org.uk/">https://migrantfamilies.nrpfnetwork.org.uk/</a></li></div>`,
+          `<div ><b>International Expat guide</b></div>`,
+          `<div ><li>Comprehensive Guide to Britain full of useful tips for migrants coming to work and live in the United Kingdom. <a href="https://www.internations.org/great-britain-expats/guide">https://www.internations.org/great-britain-expats/guide</a></li></div>`,
         ]
       }
 

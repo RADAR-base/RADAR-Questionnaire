761ae61988349164c8c69a8e5650242afb4f6031
Peyman Mohtashami <pmohtashami@localhost.localdomain>
2020-03-12 16:28:08 +0100

    Modify texts: consent and welcome pages

--- src/app/pages/auth/components/consent-page/consent-page.component.ts
+++ src/app/pages/auth/components/consent-page/consent-page.component.ts
@@ -68,7 +68,8 @@ export class ConsentPageComponent {
         detail: [
           "We know your time is valuable. That’s why the HOME app has two types of surveys - one short and one longer. Each time we want you to complete one of these surveys you will be sent a notification to open the HOME app.",
           "In the short survey, sent either once a week or every three days, you will be asked about your health in the last week. If you have been feeling well, this will take less than 15 seconds to complete. If you have not been well, it will take about 1 min to complete.",
-          "In the longer surveys, sent either once a week or every two weeks, you will be asked about a wider range of health and non-health related questions. These surveys are designed to take less than 5 minutes to complete."
+          "In the longer surveys, sent either once a week or every two weeks, you will be asked about a wider range of health and non-health related questions. These surveys are designed to take less than 5 minutes to complete.",
+          "We kindly ask you to participate for a period of 3 months."
         ]
       },
       {
@@ -82,7 +83,7 @@ export class ConsentPageComponent {
         title: "Data Collection",
         iconMd: "md-analytics",
         detail: [
-          "The survey questions will cover a wide range of topics that can affect your health and wellbeing, like your nutrition, housing, employment, and experience with your GP or healthcare provider. You may find that some questions do not apply to you or make you feel uncomfortable. You are free to choose ‘no response’ or stop answering the survey at any time by choosing the ‘stop survey’ option. Your data will only be used for academic research by UCL and will not be shared with anyone."
+          "The survey questions will cover a wide range of topics that can affect your health and wellbeing, like your nutrition, housing, employment, and experience with your GP or healthcare provider. You may find that some questions do not apply to you or make you feel uncomfortable. You are free to choose ‘no response’ or stop answering the survey at any time by choosing the ‘close’ option. Your data will only be used for academic research by UCL and will not be shared with anyone."
         ]
       },
       {
@@ -110,7 +111,7 @@ export class ConsentPageComponent {
         title: "Data Protection",
         iconMd: "md-paper",
         detail: [
-          "The controller for this project will be University College London (UCL). The UCL Data Protection Officer provides oversight of UCL activities involving the processing of personal data, and can be contacted at data-protection@ucl.ac.uk. This ‘local’ privacy notice sets out the information that applies to this particular study. Further information on how UCL uses participant information can be found in our ‘general’ privacy notice: For participants in health and care research studies, click here (www.homeappstudy.net).",
+          "The controller for this project will be University College London (UCL). The UCL Data Protection Officer provides oversight of UCL activities involving the processing of personal data, and can be contacted at data-protection@ucl.ac.uk. This ‘local’ privacy notice sets out the information that applies to this particular study. Further information on how UCL uses participant information can be found in our ‘general’ privacy notice (www.ucl.ac.uk/legal-service).”",
           "The information that is required to be provided to participants under data protection legislation (GDPR and DPA 2018) is provided across both the ‘local’ and ‘general’ privacy notices. The lawful basis that will be used to process your personal data are: ‘Public task’ for personal data and’ Research purposes’ for special category data.",
           "Your personal data will be processed so long as it is required for the research project. If we are able to anonymise or pseudonymise the personal data you provide we will undertake this, and will endeavour to minimise the processing of personal data wherever possible. If you are concerned about how your personal data is being processed, or if you would like to contact us about your rights, please contact UCL in the first instance at data-protection@ucl.ac.uk."
         ]
@@ -130,8 +131,14 @@ export class ConsentPageComponent {
         detail: [
           "Your participation in this study is entirely voluntary. You may withdraw your consent and stop your participation in this study at any time. If you choose to withdraw, we will stop collecting any new data and can destroy all of the data we have previously collected. To withdraw from the study, simply send an email to the research team at r.aldridge@ucl.ac.uk."
         ]
+      },
+      {
+        title: "Contact Us",
+        iconMd: "md-log-out",
+        detail: [
+          "If you have any questions prior to participating in the study or during the course of the study, please feel free to contact the HOME study team at all times by sending us an email at homeappstudy@ucl.ac.uk. We will reply to your message as soon as we can."
+        ]
       }
-
     ]
     this.evaluatedQuestions = new Map<number, YesOrNoQuestion>()
   }

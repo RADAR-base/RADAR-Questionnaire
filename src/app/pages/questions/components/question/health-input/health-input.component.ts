086240ef47dc279e930e5678e575f3d2f1f05784
You Kai <kaiyour@amazon.com>
2022-09-03 14:43:07 +0800

    add localstorage, platformsupport, errorhandling

--- src/app/pages/questions/components/question/health-input/health-input.component.ts
+++ src/app/pages/questions/components/question/health-input/health-input.component.ts
@@ -8,10 +8,8 @@ import { Health_Requirement, Question } from 'src/app/shared/models/question'
   styleUrls: ['health-input.component.scss']
 })
 export class HealthInputComponent implements OnInit {
-  // 1. JUDGE BY HEALTH INPUT TYPE TO DECIDE WHAT DATA WE NEED
-  // 2. PRINT OUT THE DATA AND REQUEST ACCORDINGLY
   @Output()
-  valueChange: EventEmitter<number> = new EventEmitter<number>()
+  valueChange: EventEmitter<object> = new EventEmitter<object>()
 
   @Input()
   responses: Response[]
@@ -19,39 +17,22 @@ export class HealthInputComponent implements OnInit {
   @Input()
   health_question: Question
 
-  health_requirements: Health_Requirement[] = [
-    {
-      data_name: 'weight',
-      time_intervals: '1000',
-      value: ''
-    },
-    {
-      data_name: 'height',
-      time_intervals: '1000',
-      value: ''
-    },
-    {
-      data_name: 'blood_pressure',
-      time_intervals: '1000',
-      value: ''
-    }
-  ]
+  health_value: any
+  health_display: any
+  health_display_time: any 
+  health_time: any
+  not_support = false 
+  // the interval days for first query 
+  defaultInterval = 1000
 
-  health_value: string
-  test_data: any
-
-  constructor(private health: Health) {}
+  constructor(private health: Health) { }
   ngOnInit() {
-    console.log(this.health_question)
     const requireField = [this.health_question.field_name]
-    console.log(requireField)
     this.health
       .isAvailable()
       .then((available: boolean) => {
-        console.log(available)
         this.health
           .requestAuthorization([
-            // 'distance',
             // 'nutrition', //read and write permissions
             {
               read: requireField //read only permission
@@ -59,49 +40,137 @@ export class HealthInputComponent implements OnInit {
             }
           ])
           .then(res => {
-            this.loadData()
+            this.loadData().then(() =>{
+              this.onInputChange()
+            })
           })
           .catch(e => console.log(e))
       })
-      .catch(e => console.log(e))
-    this.exportData()
-    this.onInputChange(this.test_data)
+      .catch(e => {
+        this.not_support = true 
+      })
   }
 
-  onInputChange(event) {
+  onInputChange() {
+    const event = {
+      'value' : this.health_value,
+      'time': this.health_display_time
+    }
+    console.log("Final Sumit Data: ", event)
     this.valueChange.emit(event)
   }
-  reset() {
-    this.loadData()
-  }
-  exportData() {
-    this.test_data = [
-      {
-        namespace: 'org.radarcns.connector.health',
-        type: 'record',
-        name: 'patient302',
-        doc: 'General health data for patient',
-        fields: [
-          {
-            name: 'height',
-            type: 'double',
-            doc: 'data for height, current unit will be "m"',
-            value: 'test'
-          }
-        ]
+
+  async loadData() {
+
+    // TODO: Error Handling for empty return 
+    // Done: Implement LocalStorage
+    // Done: Limit the float for UI 
+    // Done: Check Andorid device when loading the data
+      // * Still has some improvment for this UI dispaly
+
+    // TODO: Processing the data 
+
+    // !clearn the localstorage query 
+    // localStorage.setItem('lastQueryTimeDic', null)
+
+ 
+    const lastQueryTimeDic = localStorage.getItem('lastQueryTimeDic')
+    console.log("LastQuery: ", lastQueryTimeDic)
+
+    if(lastQueryTimeDic !== "null"){
+
+      const dic = JSON.parse(lastQueryTimeDic) 
+
+      console.log("Dic",dic)
+      if(this.health_question.field_name in dic){
+        console.log("Has Dic")
+        await this.query(new Date(dic[this.health_question.field_name]), new Date())
       }
-    ]
+      else{
+        await this.query(new Date(new Date().getTime() - this.defaultInterval * 24 * 60 * 60 * 1000), new Date())
+      }
+      dic[this.health_question.field_name] = new Date().toLocaleDateString()
+      localStorage.setItem('lastQueryTimeDic', JSON.stringify(dic))
+    }
+    else{ 
+
+      await this.query(new Date(new Date().getTime() - this.defaultInterval * 24 * 60 * 60 * 1000), new Date())
+      const newDic = {
+        [this.health_question.field_name]: new Date().toLocaleDateString()
+      }
+      localStorage.setItem('lastQueryTimeDic', JSON.stringify(newDic))
+    }
+    
   }
-  loadData() {
-    this.health
-      .query({
-        startDate: new Date(new Date().getTime() - 1000 * 24 * 60 * 60 * 1000), // three days ago
-        endDate: new Date(), // now
+
+  async query(queryStartDate: Date, queryEndDate: Date){
+    const aggregatedField = [ 'steps', 'distance','calories','activity', 'nutrition']
+    console.log('Start Date: ', queryStartDate.toLocaleDateString())
+    console.log('End Date: ', queryEndDate.toLocaleDateString())
+    //aggregated data 
+    if ( aggregatedField.includes(this.health_question.field_name) || this.health_question.field_name.startsWith('nutrition') || this.health_question.field_name.startsWith('calories')){
+      await this.health.queryAggregated({
+        startDate: queryStartDate, // three days ago
+        endDate: queryEndDate, // now
         dataType: this.health_question.field_name,
-        limit: 1000
+        bucket: 'day'
+      }).then(res => {
+        console.log("Value Before Updated: ", this.health_value);
+        if(res === null){
+          console.log(this.health_question.field_name, " is empty")
+          this.health_value = null
+          this.health_display = "There is no data for this date"
+        }else{
+          console.log("Field Name: ", this.health_question.field_name)
+          const value = res[res.length-1].value 
+          this.health_value = parseFloat(value)
+          this.health_display = this.health_value.toString() + " " +  res[res.length-1].unit
+        }
+        this.health_display_time = res[res.length-1].startDate.toLocaleDateString()
+        console.log("Value After Updated: ", this.health_value )
       })
-      .then(res => {
-        this.health_value = res[0].value + res[0].unit
-      })
+    }
+    else {
+      await this.health
+        .query({
+          // put the lastDate in StartDate 
+          startDate: queryStartDate,
+          endDate: queryEndDate, // now
+          dataType: this.health_question.field_name,
+          limit: 1000
+        })
+        .then(res => {
+          console.log("Value Before Updated: ", this.health_value);
+          console.log("res", res);
+          if(res.length === 0){
+            console.log(this.health_question.field_name, " is empty")
+            this.health_value = null
+            this.health_display = "No data for today"
+            this.health_display_time = new Date().toLocaleDateString()
+          }
+          else{
+            if( this.health_question.field_name === 'blood_pressure'){
+              const blood_pressure = res[0].value as any
+              this.health_value = blood_pressure.systolic + '/' + blood_pressure.diastolic
+              this.health_display = this.health_value.toFixed(2) + " " +  res[0].unit
+            }
+            else if (this.health_question.field_name === 'date_of_birth'){
+              const value = res[0].value as any
+              console.log(value.day + '/' + value.month + '/' + value.year)
+              this.health_value = value.day + '/' + value.month + '/' + value.year
+              this.health_display = this.health_value
+            }
+            else{
+              console.log("Field Name: ", this.health_question.field_name)
+              this.health_value = parseFloat(res[0].value)
+              this.health_display = this.health_value.toFixed(2) + " " +  res[0].unit
+            }
+            // deal with time 
+            console.log("Value After Updated: ", this.health_value )
+            this.health_display_time = res[0].startDate.toLocaleDateString()
+          }
+
+        })
+    }
   }
 }

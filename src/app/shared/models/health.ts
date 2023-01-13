e1cff0ecda0cc8ca30317c6211a8bd6ccff9cd84
mpgxvii <mpgxvii@gmail.com>
2023-01-13 11:39:43 +0000

    fix: healthkit enums

---
+++ src/app/shared/models/health.ts
@@ -0,0 +1,15 @@
+export enum HealthkitDataType {
+  STAIRS = 'stairs',
+  DISTANCE = 'distance',
+  APPLE_EXERCISE_TIME = 'appleExerciseTime',
+  VO2MAX = 'vo2Max',
+  ACTIVITY = 'activity'
+}
+
+export enum HealthkitSchemaType {
+  ACTIVITY = 'healthkit_activity',
+  BODY_MEASUREMENT = 'healthkit_body_measurement',
+  CALORIES = 'healthkit_calories',
+  HEART_RATE = 'healthkit_heart_rate',
+  VITAL_SIGNS = 'healthkit_vital_signs'
+}

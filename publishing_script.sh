3ae4636a8ea8da1a134ac6ce1c54cd9390c7e499
mpgxvii <mpgxvii@gmail.com>
2023-06-22 12:53:04 +0100

    fix: update publishing script

--- publishing_script.sh
+++ publishing_script.sh
@@ -1,6 +1,5 @@
-ionic cordova build --release android
+ionic cordova build android --release -- -- --packageType=apk
 
-~/Library/Android/sdk/build-tools/28.0.3/zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk ~/Downloads/radar-armt-app-zipaligned.apk
+~/Library/Android/sdk/build-tools/32.0.0/zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk ~/Downloads/radar-armt-app-zipaligned.apk
 
-~/Library/Android/sdk/build-tools/28.0.3/apksigner sign -v --out ~/Downloads/radar-armt-app-$1.apk --ks ~/Downloads/radar-armt-release-key.keystore --ks-key-alias alias_name ~/Downloads/radar-armt-app-zipaligned.apk
-
+~/Library/Android/sdk/build-tools/32.0.0/apksigner sign -v --out ~/Downloads/radar-armt-app-$1.apk --ks ~/Downloads/radar-armt-release-key.keystore --ks-key-alias alias_name ~/Downloads/radar-armt-app-zipaligned.apk

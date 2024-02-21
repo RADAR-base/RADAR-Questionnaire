aa902bfbe083dfa97766bdde3bedb155a8181a99
Pauline <mpgxvii@gmail.com>
2024-02-21 17:05:34 +0000

    fix(enrolment): qr code scanner

--- src/app/pages/auth/components/qr-form/qr-form.component.ts
+++ src/app/pages/auth/components/qr-form/qr-form.component.ts
@@ -15,15 +15,22 @@ export class QRFormComponent {
 
   constructor() {}
 
-  scanQRHandler() {
-    const scanOptions = {
-      showFlipCameraButton: true,
-      orientation: 'portrait'
+  async scanQRHandler() {
+    document.querySelector('body').classList.add('scanner-active')
+    // Check camera permission
+    // This is just a simple example, check out the better checks below
+    await BarcodeScanner.checkPermission({ force: true })
+
+    // make background of WebView transparent
+    // note: if you are using ionic this might not be enough, check below
+    BarcodeScanner.hideBackground()
+
+    const result = await BarcodeScanner.startScan() // start scanning and wait for a result
+
+    // if the result has content
+    if (result.hasContent) {
+      this.data.emit(result.content)
     }
-    BarcodeScanner.checkPermission({ force: true }).then(() => {
-      BarcodeScanner.hideBackground().then(() =>
-        BarcodeScanner.startScan().then(res => this.data.emit(res.content))
-      )
-    })
+    document.querySelector('body').classList.remove('scanner-active');
   }
 }

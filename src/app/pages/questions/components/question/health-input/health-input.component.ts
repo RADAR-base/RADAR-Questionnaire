3791944ef64071f9662f902a7b215aa74e1ee37d
Pauline <mpgxvii@gmail.com>
2024-02-29 16:13:03 +0000

    fix: health input

--- src/app/pages/questions/components/question/health-input/health-input.component.ts
+++ src/app/pages/questions/components/question/health-input/health-input.component.ts
@@ -1,14 +1,4 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
-import {
-  ActivityData,
-  CapacitorHealthkit,
-  OtherData,
-  QueryOutput,
-  SampleNames,
-  SleepData
-} from '@perfood/capacitor-healthkit'
-import { StorageService } from 'src/app/core/services/storage/storage.service'
-import { StorageKeys } from 'src/app/shared/enums/storage'
 import { Health_Requirement, Question } from 'src/app/shared/models/question'
 
 import { Response } from '../../../../../shared/models/question'
@@ -31,137 +21,28 @@ export class HealthInputComponent implements OnInit {
 
   health_display: string
   health_display_time: string
-  health_value: any
-  health_time: any
   isSupported = false
 
-  // the bucket for aggregated query
-  defaultInterval = 7 // days
-  defaultBucket = 'day'
-  MIN_POLL_TIMESTAMP = new Date(
-    new Date().getTime() - this.defaultInterval * 24 * 60 * 60 * 1000
-  )
-  READ_PERMISSIONS = [
-    'calories',
-    'stairs',
-    'activity',
-    'steps',
-    'distance',
-    'duration',
-    'weight'
-  ]
-
-  constructor(private storage: StorageService) {}
+  constructor(private healthKitService: HealthkitService) {}
 
   ngOnInit() {
-    this.initLastPollTimes()
-    let requireField = []
-    if (
-      this.health_question.field_name === 'blood_pressure_systolic' ||
-      this.health_question.field_name === 'blood_pressure_diastolic'
-    ) {
-      requireField = ['blood_pressure']
-    } else {
-      requireField = [this.health_question.field_name]
-    }
-    const healthDataType = requireField[0]
-    CapacitorHealthkit.isAvailable()
-      .then(() => {
-        CapacitorHealthkit.requestAuthorization({
-          all: [''],
-          read: requireField,
-          write: ['']
-        })
-          .then(res => {
-            this.loadData(healthDataType).then(data => {
-              this.onInputChange(data)
-            })
-          })
-          .catch(e => console.log(e))
-      })
-      .catch(e => {
-        this.isSupported = false
-      })
+    this.loadData()
   }
 
-  initLastPollTimes() {
-    const dic = this.storage.get(StorageKeys.HEALTH_LAST_POLL_TIMES)
-    if (!dic) {
-      this.storage.set(StorageKeys.HEALTH_LAST_POLL_TIMES, {})
-    }
-  }
-
-  onInputChange(data) {
-    const event = {
-      value: this.health_value,
-      time: this.health_time
-    }
-    this.valueChange.emit(data)
-  }
-
-  loadData(healthDataType) {
-    const dic = this.storage.get(StorageKeys.HEALTH_LAST_POLL_TIMES)
-    const lastPollTime = new Date(dic[healthDataType])
-
-    return CapacitorHealthkit.requestAuthorization({
-      all: [''],
-      read: [healthDataType],
-      write: ['']
-    })
-      .then(() => {
-        return this.query(
-          lastPollTime ? lastPollTime : this.MIN_POLL_TIMESTAMP,
-          new Date(),
-          healthDataType
-        ).then(res => {
-          dic[healthDataType] = new Date().toLocaleDateString()
-          this.storage.set(StorageKeys.HEALTH_LAST_POLL_TIMES, dic)
-          return res
-        })
-      })
-      .catch(e => {
-        console.log(e)
-        return null
-      })
-  }
-
-  query(queryStartTime: Date, queryEndTime: Date, dataType: string) {
-    // !Will have to remove activity here, since each activity acutally contains more payload
-    // !Set the acitiviy to be UNKNOWN for now to avoid schema confliction
-    return CapacitorHealthkit.queryHKitSampleType<OtherData>({
-      // put the lastDate in StartDate
-      startDate: queryStartTime.toISOString(),
-      endDate: queryEndTime.toISOString(), // now
-      sampleName: dataType,
-      limit: 1000
-    }).then((data: QueryOutput<OtherData>) => {
-      console.log('Field type: ' + dataType)
-      const countReturn = data.countReturn
-      const res = data.resultData
-      if (countReturn === 0) {
-        this.health_value = null
-        this.health_display = 'No data for today'
+  loadData() {
+    let healthDataType = this.health_question.field_name
+    if (this.health_question.field_name.includes('blood_pressure'))
+      healthDataType = 'blood_pressure'
+    this.healthKitService.checkHealthkitSupported().then(() => {
+      this.isSupported = true
+      if (this.isSupported) {
         this.health_display_time = new Date().toLocaleDateString()
-        this.health_time = Math.floor(
-          new Date(res[0].startDate).getTime() / 1000
-        )
-      } else {
-        if (dataType === 'date_of_birth') {
-          const value = res[0].value as any
-          this.health_value = value.day + '/' + value.month + '/' + value.year
-          this.health_display = this.health_value
-        } else {
-          this.health_value = res[0].value
-          this.health_display =
-            this.health_value.toFixed(2) + ' ' + res[0].unitName
-        }
-        // deal with time
-        this.health_display_time = res[0].startDate
-        this.health_time = Math.floor(
-          new Date(res[0].startDate).getTime() / 1000
-        )
+        this.health_display = 'Loading..'
+        this.healthKitService.loadData(healthDataType).then(data => {
+          this.health_display = data.length + ' records'
+          return this.valueChange.emit(data)
+        })
       }
-      return res
-    })
+    }).catch(e => this.isSupported = false)
   }
 }

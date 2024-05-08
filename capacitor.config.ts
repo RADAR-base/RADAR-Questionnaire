7bc302080e05d41ec856e25024eee237fb09b4a2
Pauline <mpgxvii@gmail.com>
2024-05-08 20:49:18 +0100

    fix: names and deps

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
   appId: 'org.phidatalab.radar_armt',
-  appName: 'RADAR Questionnaire',
+  appName: 'RADAR Active RMT',
   webDir: 'www',
   server: {
     androidScheme: 'https'

be6dc641d9644626fbf88c1943bd37b99aea8568
Pauline <mpgxvii@gmail.com>
2025-02-06 13:55:08 +0000

    Check all questions are answered in matrix

--- src/app/pages/questions/containers/questions-page.component.ts
+++ src/app/pages/questions/containers/questions-page.component.ts
@@ -280,7 +280,7 @@ export class QuestionsPageComponent implements OnInit {
     const currentQs = this.getCurrentQuestions()
     if (!currentQs) return
     this.isRightButtonDisabled =
-      !this.questionsService.isAnyAnswered(currentQs) &&
+      !this.questionsService.areAllAnswered(currentQs) &&
       !this.questionsService.getIsAnyNextEnabled(currentQs)
     this.isLeftButtonDisabled =
       this.questionsService.getIsAnyPreviousEnabled(currentQs)

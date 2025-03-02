6390800ed2e913795e95b5bae8fda2fe97f9d2af
Peyman Mohtashami <p.mohtashami@gmail.com>
2025-03-02 16:00:26 +0100

    Fix NaN comparison in sort-object.ts

--- src/app/shared/utilities/sort-object.ts
+++ src/app/shared/utilities/sort-object.ts
@@ -5,7 +5,7 @@ export function sortObject(object, order) {
   return Object.keys(object)
     .sort((a, b) => {
       const diff = order[a] - order[b]
-      if (diff == 0 || diff == NaN) return a < b ? -1 : a > b ? 1 : 0
+      if (diff == 0 || isNaN(diff)) return a < b ? -1 : a > b ? 1 : 0
       else return diff < 0 ? -1 : 1
     })
     .reduce((acc, key) => {
